c56e9dc56fd81d0e1c1aad3b592f8047761b3980
Rodrigues L. Daniel <daniel.24.rodrigues@gmail.com>
2016-06-01 08:19:58 +0000

    Etablissement de la communication entre Bot<->Slack et Bot<->TMDB_API.

--- movieBot/__init__.py
+++ movieBot/__init__.py
@@ -1,100 +1,147 @@
 """Slack bot using asyncio"""
+
 import asyncio
 import json
 import signal
 import aiohttp
-import websockets
+import websocket
+import urllib3
 
-from movieBot.config import DEBUG, TOKEN, TMDB_KEY
+from moviebot.config import DEBUG, TOKEN, TMDB_KEY
 
 RUNNING = True
 
-# Example of query 
-# query = "roi"
-#
-# request = Request('http://api.themoviedb.org/3/search/$s?query=$s&api_key=$s' (movie, query, self.tmdb_key, headers=headers)
-#
-# with urlopen(request) as f:
-# 	js = json.loads(f.read().decode('utf-8'))
-# 	print(js['results'][0])
+# URL for poster
+# https://image.tmdb.org/t/p/w396/
 
 class MovieBot:
-	def __init__(self, token=TOKEN, tmdb_key = TMDB_KEY):
-		self.token = token
+
+    def __init__(self, token=TOKEN, tmdb_key = TMDB_KEY):
+        self.token = token
         self.rtm = None
-		self.tmdb_key = tmdb_key
-		self.headers = { 'Accept': 'application/json' }
+        self.cmd = {
+            "help" : self.help,
+            "movie" : self.searchMovie,
+            "person" : self.searchPerson,
+            "series" : self.searchSeries
+        }
+        self.tmdb_key = tmdb_key
+        self.headers = { 'Accept': 'application/json' }
+        self.url = "http://api.themoviedb.org/3/search/{0}?query={1}&api_key={2}"
+        self.http = urllib3.PoolManager()
 
-	async def help(self):
-		"""Display help Message"""
-		pass
 
-	async def searchMovie():
-		"""Search a movie."""
-		pass
-		
-	async def searchPerson():
-		"""Search a person."""
-		pass
-		
-	async def searchSeries():
-		"""Serach a serie."""
-		pass
-		
-    async def send(self, message):
+    async def help(self, channel_id, team_id, query):
+        """Display help message"""
+        helpMsg = "Movie Bot help !"
+        return await self.send(helpMsg, channel_id, team_id)
+
+
+    async def searchMovie(self, channel_id, team_id, query):
+        """Search a movie."""
+        r = self.http.request('GET', self.url.format("movie", query, self.tmdb_key), headers=self.headers)
+        js = json.loads(r.data.decode('utf-8'))
+        return await self.send(js['results'][0], channel_id, team_id)
+
+
+    async def searchPerson(self, channel_id, team_id, query):
+        """Search a person."""
+        r = self.http.request('GET', self.url.format("person", query, self.tmdb_key), headers=self.headers)
+        js = json.loads(r.data.decode('utf-8'))
+        return await self.send(js['results'][0], channel_id, team_id)
+
+
+    async def searchSeries(self, channel_id, team_id, query):
+        """Serach a serie."""
+        r = self.http.request('GET', self.url.format("tv", query, self.tmdb_key), headers=self.headers)
+        js = json.loads(r.data.decode('utf-8'))
+        return await self.send(js['results'][0], channel_id, team_id)
+
+
+    async def send(self, message, channel_id, team_id):
         """Sending message to Slack."""
-        pass
+        return await api_call('chat.postMessage', {"type": "message",
+                                                   "channel": channel_id,
+                                                   "text": "{0}".format(message),
+                                                   "team": team_id})
+
 
     async def receive(self, message):
-        """Receive message from Slack"""
         pass
+        """Receive message from Slack"""
+        if message.get('type') == 'message':
 
-#    async def connect(self):
-#		"""Joins Slack."""
-#		self.rtm = await api_call("rtm.start")
-#		assert self.rtm['ok'], "Error connecting to RTM."
-#
-#		async with aiohttp.ClientSession() as session:
-#			async with session.ws_connect(self.rtm["url"]) as ws:
-#				async for msg in ws:
-#					assert msg.tp == aiohttp.MsgType.text
-#					message = json.loads(msg.data)
-#					asyncio.ensure_future(selt.receive(message))
+            # Channel ID
+            channel_id = message.get('channel')
 
+            # Team ID
+            team_id = self.rtm['team']['id']
 
-#async def api_call(method, data=None, file=None, token=TOKEN):
-#    """Perform an API call to Slack.
-#    :param method: Slack API method name.
-#    :param type: str
-#    :param data: Form data to be sent.
-#    :param type: dict
-#    :param file: file pointer to send (for files.upload).
-#    :param type: file
-#    :param token: OAuth2 tokn
-#    :param type: str
-#    """
-#    with aiohttp.ClientSession() as session:
-#        form = aiohttp.FormData(data or {})
-#        form.add_field("token", token)
-#        if file:
-#            form.add_field("file", file)
-#        async with session.post('https://slack.com/api/{0}'.format(method),
-#                                data=form) as response:
-#            assert 200 == response.status, ("{0} with {1} failed."
-#                                            .format(method, data))
-#            return await response.json()
+            # Bot ID
+            bot_id = self.rtm['self']['id']
+
+            # THE Message
+            message_text = message.get('text')
+
+            # Splits message in half, with recipient on the left side, and the core text on the other.
+            if (isinstance(message_text, str)):
+                message_split = message_text.split(':', 1)
+                recipient = message_split[0].strip()
+
+                # Check if message is adressed to this bot
+                if len(message_split) > 0 and recipient == '<@{0}>'.format(bot_id):
+                    core_text = message_split[1].strip()
+                    core_text_split = core_text.split()
+                    command = core_text_split[0].strip()
+                    query = core_text_split[1].strip()
+                    action = self.cmd.get(command) or self.help
+                    await action(channel_id, team_id, query)
 
 
-#def stop():
-#    """Gracefully stop the bot."""
-#    global RUNNING
-#    RUNNING = False
-#    print("Stopping... closing connections.")
+    async def connect(self):
+        """Joins Slack."""
+        self.rtm = await api_call("rtm.start")
+        assert self.rtm['ok'], "Error connecting to RTM."
+
+        with aiohttp.ClientSession() as session:
+            async with session.ws_connect(self.rtm["url"]) as ws:
+                async for msg in ws:
+                    assert msg.tp == aiohttp.MsgType.text
+                    message = json.loads(msg.data)
+                    asyncio.ensure_future(self.receive(message))
 
 
-#if __name__ == "__main__":
-#    loop = asyncio.get_event_loop()
-#    loop.set_debug(DEBUG)
-#    bot = MovieBot(TOKEN, TMDB_KEY)
-#    loop.run_until_complete(asyncio.wait(bot.connect())
-#    loop.close()
+async def api_call(method, data=None, file=None, token=TOKEN):
+    """Perform an API call to Slack.
+   :param method: Slack API method name.
+   :param type: str
+   :param data: Form data to be sent.
+   :param type: dict
+   :param file: file pointer to send (for files.upload).
+   :param type: file
+   :param token: OAuth2 tokn
+   :param type: str
+   """
+    with aiohttp.ClientSession() as session:
+        form = aiohttp.FormData(data or {})
+        form.add_field("token", token)
+        if file:
+            form.add_field("file", file)
+        async with session.post('https://slack.com/api/{0}'.format(method), data=form) as response:
+            assert 200 == response.status, ("{0} with {1} failed.".format(method, data))
+            return await response.json()
+
+
+def stop():
+   """Gracefully stop the bot."""
+   global RUNNING
+   RUNNING = False
+   print("Stopping... closing connections.")
+
+
+if __name__ == "__main__":
+   loop = asyncio.get_event_loop()
+   loop.set_debug(DEBUG)
+   bot = MovieBot(TOKEN, TMDB_KEY)
+   loop.run_until_complete(bot.connect())
+   loop.close()
