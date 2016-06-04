03e46e082e9dd562129236e741b1104e4307805d
Rodrigues L. Daniel <daniel.24.rodrigues@gmail.com>
2016-06-04 12:02:11 +0000

    Ajout des tests.

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -1,10 +1,23 @@
-"""Test movieBot"""
+"""Test moviebot"""
+
 import moviebot
 import pytest
+import os
+
 
 @pytest.fixture()
 def bot():
-	return moviebot.MovieBot()
+    token = os.environ.get('TOKEN')
+    api_key = os.environ.get('API_KEY')
+    if not token.startswith('xoxb-'):
+        return "token not defined."
+    return moviebot.bot.MovieBot(token, api_key)
 
-def test_bot_receive(bot):
-	pass
+
+@pytest.mark.asyncio
+async def test_bot_parsefilm(bot):
+    """Test the bot's method parsefilm()"""
+    res = await bot.parseFilm({'title': 'titi', 'overview': 'Il fait beau', 'release_date': '04-06-2016', 'poster_path': 'titiPoster.jpg', 'original:language': 'fr', 'vote_average': '5.0'})
+    assert res[0]['title'] == 'titi - 04-06-2016 - 5.0/10'
+    assert res[0]['text'] == 'Il fait beau'
+    assert res[0]['image_url'] == 'https://image.tmdb.org/t/p/w396/titiPoster.jpg'
